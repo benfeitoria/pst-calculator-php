4ed41295fe8f75fd3f781f27217430675ba9324d
Jorge Junior <jorge.junior.568@live.com>
2019-07-26 17:52:06 -0300

    Calculator for percentage

--- src/PSP.php
+++ src/PSP.php
@@ -11,5 +11,70 @@ namespace PSP;
 
 class PSP
 {
+    /**
+     * @var array
+     */
+    private $investedValues;
 
+    /**
+     * PSP constructor.
+     * @param array $investedValues
+     */
+    public function __construct(array $investedValues)
+    {
+
+        $this->investedValues = $investedValues;
+    }
+
+    /**
+     * @return array
+     */
+    public function getInvestedValues(): array
+    {
+        return $this->investedValues;
+    }
+
+    /**
+     * @param array $investedValues
+     */
+    public function setInvestedValues(array $investedValues): void
+    {
+        $this->investedValues = $investedValues;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotalInvestedValue(){
+        return array_sum($this->investedValues);
+    }
+
+    /**
+     * @param float $percentage
+     * @return float|int
+     */
+    public function getTotalInvestedValueDisconsideringHighestThanPercentageFromTotal(float $percentage){
+        $totalInvested = $this->getTotalInvestedValue();
+
+        return array_sum(
+            array_filter($this->investedValues,function($investedValue) use($totalInvested,$percentage){
+                return !$this->isHigherThanPercentageForTotal($investedValue,$totalInvested,$percentage);
+            })
+        );
+    }
+
+    /**
+     * @param $investedValue
+     * @param $totalInvested
+     * @param $percentage
+     * @return bool
+     */
+    private function isHigherThanPercentageForTotal($investedValue, $totalInvested, $percentage){
+        $percentageForTotal = $totalInvested * $percentage;
+        return $investedValue > $percentageForTotal;
+    }
+
+    public static function make(array $investedValues){
+        return new self($investedValues);
+    }
 }
